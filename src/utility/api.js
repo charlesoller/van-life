edf8a1d631553a658557ab5fba86c31ac39b4f31
charlesoller <charlesrello@gmail.com>
2023-10-22 17:10:26 -0400

    converting to Firebase

--- src/utility/api.js
+++ src/utility/api.js
@@ -1,20 +1,33 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(() => resolve(), ms))
-}
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore/lite"
 
-export async function getVans(id) {
-    const url = id ? `/api/vans/${id}` : "/api/vans"
-    const res = await fetch(url)
-    if (!res.ok) {
-        throw {
-            message: "Failed to fetch vans",
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-    const data = await res.json()
-    return data.vans
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyAuAq-c0fDrCv1iZngwphzSDWxeq_pJDRA",
+  authDomain: "vanlife-c45cc.firebaseapp.com",
+  projectId: "vanlife-c45cc",
+  storageBucket: "vanlife-c45cc.appspot.com",
+  messagingSenderId: "371164399703",
+  appId: "1:371164399703:web:d0c0fe4962b91fb064bb12"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
+// export async function getVans(id) {
+//     const url = id ? `/api/vans/${id}` : "/api/vans"
+//     const res = await fetch(url)
+//     if (!res.ok) {
+//         throw {
+//             message: "Failed to fetch vans",
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+//     const data = await res.json()
+//     return data.vans
+// }
 
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
